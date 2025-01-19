5429706529a373d2aa5bacf5887268ea1239ac0c
Ermek <65ermek@gmail.com>
2025-01-19 11:58:34 +0100

    Build and prepare for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   base: '/shop/',
-  transpileDependencies: true,
-})
-module.exports = {
-  configureWebpack: {
-    devtool: 'source-map', // Убедитесь, что добавлена карта кода для отладки
-  },
-  productionSourceMap: true, // Включите source map для production
-};
+});
+
